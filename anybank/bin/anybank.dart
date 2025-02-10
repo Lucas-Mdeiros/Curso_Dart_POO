67625b257b83a02b40140589e0f0fa80be891c9d
Lucas Medeiros <lucas_mdeiros@outlook.com>
2025-02-10 09:36:30 -0300

    Criar classes contaBanco

--- anybank/bin/anybank.dart
+++ anybank/bin/anybank.dart
@@ -1,3 +1,18 @@
 void main() {
+  Conta contaMatheus = Conta("Matheus", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
 
+  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
+  for(Conta conta in contas){
+    print(conta.titular);
+  }
+
+}
+
+
+class Conta{
+  String titular;
+  double saldo;
+
+  Conta(this.titular, this.saldo);
 }
