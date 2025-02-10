837819f4424885aa38938835f6acf8e236c9210c
Lucas Medeiros <lucas_mdeiros@outlook.com>
2025-02-10 17:20:39 -0300

    Criar objeto privado

--- anybank/bin/anybank.dart
+++ anybank/bin/anybank.dart
@@ -1,18 +1,16 @@
+import 'package:anybank/conta.dart';
+
 void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
 
   List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
   for(Conta conta in contas){
-    print(conta.titular);
+    conta.imprimeSaldo();
   }
 
-}
+  contaMatheus.receber(500);
 
-
-class Conta{
-  String titular;
-  double saldo;
-
-  Conta(this.titular, this.saldo);
+  contaRoberta.enviar(500);
+  
 }
---
+++ anybank/lib/conta.dart
@@ -0,0 +1,24 @@
+class Conta{
+  String titular;
+  double _saldo;
+  Conta(this.titular, this._saldo);
+
+  void enviar(double valor){
+    if(valor < _saldo){
+      _saldo -= valor;
+      imprimeSaldo();
+    }
+    
+    
+  }
+  void receber(double valor){
+    _saldo += valor;
+    imprimeSaldo();
+  }
+
+
+  void imprimeSaldo(){
+    print("O saldo atual de $titular é: R\$$_saldo");
+  }
+  
+}
