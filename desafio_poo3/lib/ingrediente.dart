ad531f9250b803f59c08f04927ad1a0ff01f7afc
Lucas-Mdeiros <lucas_mdeiros@outlook.com>
2025-02-11 21:11:08 -0300

    Desafios 7 e 8 pt 3 concluidos

---
+++ desafio_poo3/lib/ingrediente.dart
@@ -0,0 +1,44 @@
+class Ingrediente {
+  String nome;
+  String tipo;
+
+  Ingrediente(this.nome, this.tipo);
+
+  void detalhes(){
+    print("Nome: $nome");
+    print("Tipo: $tipo");
+  }
+}
+
+
+class Fruta extends Ingrediente{
+  Fruta(super.nome, super.tipo);
+
+  @override
+  void detalhes(){
+    print("Nome: $nome");
+    print("Tipo: $tipo");
+    print("Em geral, são vegetais que não podem ser cozidos nas receitas");
+  }
+}
+
+class Legume extends Ingrediente{
+  Legume(super.nome, super.tipo);
+    @override
+  void detalhes(){
+    print("Nome: $nome");
+    print("Tipo: $tipo");
+    print("Em geral, são vegetais que precisam ser cozidos nas receitas");
+  }
+}
+
+class Tempero extends Ingrediente{
+  Tempero(super.nome, super.tipo);
+
+    @override
+  void detalhes(){
+    print("Nome: $nome");
+    print("Tipo: $tipo");
+    print("Em geral, são usados para condimentar os alimentos");
+  }
+}
